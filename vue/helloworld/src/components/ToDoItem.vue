02279c6882ab79e44852d729f7a7701ab26c7533
FCF5646448 <2998000457@qq.com>
2023-08-31 00:08:52 +0800

    Vue：文档学习

--- vue/helloworld/src/components/ToDoItem.vue
+++ vue/helloworld/src/components/ToDoItem.vue
@@ -1,6 +1,6 @@
 <template>
-    <input type="checkbox" id="id" :checked="isDone" />
-    <label for="id">{{label}}</label>
+    <input type="checkbox" :id="id" :checked="isDone" />
+    <label :for="id">{{label}}</label>
 </template>
 
 <script>
---
+++ vue/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>vue学习</h1>
+    <p>message: {{ message }} </p>
+    <p>{{ rawHtml }}</p>
+    <p v-html="rawHtml"></p>
+    <div v-bind:id="dynamicId"></div>
+    <p>{{ num + 10 }}</p>
+    <p>{{ ok ? 'Yes': 'No' }} </p>
+    <p>{{ message.split('').reverse().join(',') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message: "学习",
+      rawHtml: "<a href='http://www.itbaizhan.com/'>百战</a>",
+      dynamicId: 10001,
+      num: 10,
+      ok: false,
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
